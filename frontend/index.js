b4817b1900ecf5830a395231fe0c62fa9e2714b0
Ashish Vishwanath <66671856+ashishv120@users.noreply.github.com>
2025-06-09 13:51:14 +0530

    Update index.js to use latest ip

--- frontend/index.js
+++ frontend/index.js
@@ -4,7 +4,7 @@ function App() {
   const [msg, setMsg] = useState("Loading...");
 
   useEffect(() => {
-    fetch("http://13.233.117.225:8000/")
+    fetch("http://65.2.82.242:8000/")
       .then(res => res.json())
       .then(data => setMsg(data.message));
   }, []);
